4e5ccc992bf5fa22ca497b19d638169b91e9653e
Dan Spencer <danrspen@gmail.com>
2021-09-15 23:26:44 +0100

    Fixed problem with README in published artefact (hopefully)

--- assert_fn/build.rs
+++ assert_fn/build.rs
@@ -1,22 +1,30 @@
 use std::fs;
 
 fn main() {
-    // Cargo publish moves the project to "target/package" before building it there, so we need
-    // to handle the different paths to the README.md
-    let readme_path = if env!("CARGO_MANIFEST_DIR").contains("target/package") {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/../../../README.md")
-    } else {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/../README.md")
-    };
+    let is_publishing = env!("CARGO_MANIFEST_DIR").contains("target/package");
 
-    println!("cargo:rerun-if-changed={}", readme_path);
+    let normal_readme_path = concat!(env!("CARGO_MANIFEST_DIR"), "/../README.md");
+    let publishing_readme_path = concat!(env!("CARGO_MANIFEST_DIR"), "/../../../README.md");
+    let published_readme_path = concat!(env!("CARGO_MANIFEST_DIR"), "/README.md");
 
-    let readme = fs::read_to_string(readme_path)
-        .unwrap_or_else(|_| panic!("Could not read {}", readme_path));
+    println!("cargo:rerun-if-changed={}", normal_readme_path);
+
+    let readme = fs::read_to_string(normal_readme_path)
+        .or_else(|_| fs::read_to_string(publishing_readme_path))
+        .or_else(|_| fs::read_to_string(published_readme_path))
+        .expect("Could not find README.md");
+
     let target_path = format!(
         "{}/README.md",
         std::env::var("OUT_DIR").expect("OUT_DIR not set")
     );
-    fs::write(target_path.clone(), readme)
+    fs::write(target_path.clone(), readme.clone())
         .unwrap_or_else(|_| panic!("Could not write README.md to {}", target_path));
+
+    if is_publishing {
+        // If we're publishing move it into the published path so that it can be found in the
+        // published artefact
+        fs::write(target_path.clone(), readme)
+            .unwrap_or_else(|_| panic!("Could not write README.md to {}", published_readme_path));
+    }
 }
