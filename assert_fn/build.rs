d2ef81d952ed961bf6d0901b60e9f515d996f32a
Dan Spencer <danrspen@gmail.com>
2021-09-15 22:58:22 +0100

    Fixed an issue with 'cargo publish' due to included readme

--- assert_fn/build.rs
+++ assert_fn/build.rs
@@ -1,11 +1,17 @@
 use std::fs;
 
-/// Because the README is a level up cargo publish refuses to publish when it is referenced in
-/// lib.rs; so we copy it into the outdir during the build.
 fn main() {
-    println!("cargo:rerun-if-changed=series");
+    // Cargo publish moves the project to "target/package" before building it there, so we need
+    // to handle the different paths to the README.md
+    let readme_path = if env!("CARGO_MANIFEST_DIR").contains("target/package") {
+        concat!(env!("CARGO_MANIFEST_DIR"), "/../../../README.md")
+    } else {
+        concat!(env!("CARGO_MANIFEST_DIR"), "/../README.md")
+    };
 
-    let readme = fs::read_to_string("../README.md").expect("Could not read README.md");
+    println!("cargo:rerun-if-changed={}", readme_path);
+
+    let readme = fs::read_to_string(readme_path).unwrap_or_else(|_| panic!("Could not read {}", readme_path));
     let target_path = format!("{}/README.md", std::env::var("OUT_DIR").expect("OUT_DIR not set"));
-    fs::write(target_path.clone(), readme).expect(&format!("Could not write README.md to {}",target_path));
+    fs::write(target_path.clone(), readme).unwrap_or_else(|_| panic!("Could not write README.md to {}",target_path));
 }
